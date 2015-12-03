c16f79c925bb8aea32d61523298fc853478d773f
briantfloyd <briantfloyd@gmail.com>
2015-12-03 08:29:42 -0800

    Remove deleted files

--- UnderROT/glyph.js
+++
@@ -1,5 +0,0 @@
-/* Copyright (c) 2015, Brian T. Floyd. FreeBSD License. */
-Game.Glyph = function(properties) {
-    properties = properties || {};
-    this.character = properties['character'] || ' ';
-};
